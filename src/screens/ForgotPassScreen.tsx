a88b2f11a0a1e827d7778581d55325ba5c20e6cc
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-10-03 00:49:20 +0700

    Update ForgotPass

--- src/screens/ForgotPassScreen.tsx
+++ src/screens/ForgotPassScreen.tsx
@@ -1,12 +1,90 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
 
-export default class ForgotPassScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
+import Toast from "react-native-toast-message";
+import { userRetryPassword } from "../api/api";
+
+const ForgotPassScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const [email, setEmail] = useState<string>("");
+
+  const handleEmailChange = (email: string) => {
+    setEmail(email);
+  };
+
+  const handleSubmit = async () => {
+    const retry: any = await userRetryPassword(email);
+    console.log("🚀 ~ handleSubmit ~ retry:", retry);
+    if (retry && retry.data) {
+      const { data } = retry as any;
+      navigation.navigate("Code", {
+        email: data.email,
+        active: 2, // quên mật khẩu
+      });
+      console.log(1233);
+    } else {
+      const { message } = retry as any;
+      Toast.show({
+        type: "error",
+        text1: message,
+      });
+    }
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} className="bg-second ">
+      <View className="relative h-screen">
+        <View className="relative w-full h-[380px] bg-white rounded-b-[30px] flex flex-col justify-center items-center">
+          <View>
+            <Text className="text-5xl text-main">Cloths</Text>
+            <Text className="text-5xl text-main">for Everyone</Text>
+          </View>
+          <View className=" absolute flex flex-row items-center justify-between bottom-0">
+            <View className="w-[134px] py-3 border-b-2 border-main">
+              <Text className="text-center text-main text-base font-semibold">
+                Forgot Password
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View className="flex gap-[30px] flex-col items-center justify-center px-[50px] mt-5">
+          <View className="flex flex-col items-center justify-between w-full space-y-1">
+            <Text className="text-sm">Enter your email address to request</Text>
+            <Text className="text-sm">a password reset.</Text>
+          </View>
+          <View className="flex space-y-1 flex-col border-b-2 w-full h-[60px]">
+            <Text className="text-sm opacity-40">Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={handleEmailChange}
+              className="tracking-wider text-base"
+            ></TextInput>
+          </View>
+        </View>
+        <View className="absolute bottom-0 w-full mb-10">
+          <View className="px-[30px]">
+            <View className="bg-main py-4 rounded-[30px]">
+              <TouchableOpacity onPress={handleSubmit}>
+                <Text className="font-bold text-xl text-white text-center">
+                  Continue
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        {/* <View className="absolute bottom-0 w-[314px]"></View> */}
       </View>
-    );
-  }
-}
+    </ScrollView>
+  );
+};
+
+export default ForgotPassScreen;
